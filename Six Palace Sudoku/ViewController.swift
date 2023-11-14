aef1a4bc7d73dec4013c61a088d37ea1f6f1d7cf
ladiesman218 <duncej@gmail.com>
2023-11-15 01:16:19 +0800

    Initial Commit

---
+++ Six Palace Sudoku/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Six Palace Sudoku
+//
+//  Created by Lei Gao on 2023/11/15.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
